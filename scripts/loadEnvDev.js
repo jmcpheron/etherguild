4543fbd3a10dc52081f0d8ced37692075986f8f8
Tobias Leinss <tobias@leinss.xyz>
2025-02-07 10:27:47 +0000

    Add .env.example, loadEnv scripts

---
+++ scripts/loadEnvDev.js
@@ -0,0 +1,2 @@
+require("dotenv").config({ path: ".env.development" });
+console.log("Environment variables loaded from .env.development");
---
+++ scripts/loadEnvProd.js
@@ -0,0 +1,2 @@
+require("dotenv").config({ path: ".env.production" });
+console.log("Environment variables loaded from .env.production");
