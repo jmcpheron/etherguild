4f3643ac24cc614571b859cc71a13bc1137c3bdd
Kevin Weaver <kevinweaver2@gmail.com>
2025-04-02 22:15:26 -0700

    force dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,8 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem
+          forcedTheme="dark"
+          //enableSystem
           disableTransitionOnChange
         >
           <Providers>
